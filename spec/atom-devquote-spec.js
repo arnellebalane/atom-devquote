8201cda98975663cc292f6f5368fe0903f748deb
Arnelle Balane <arnellebalane@gmail.com>
2016-05-06 18:19:50 +0800

    change indentation width to four spaces

--- spec/atom-devquote-spec.js
+++ spec/atom-devquote-spec.js
@@ -8,66 +8,66 @@ import AtomDevquote from '../lib/atom-devquote';
 // or `fdescribe`). Remove the `f` to unfocus the block.
 
 describe('AtomDevquote', () => {
-  let workspaceElement, activationPromise;
+    let workspaceElement, activationPromise;
 
-  beforeEach(() => {
-    workspaceElement = atom.views.getView(atom.workspace);
-    activationPromise = atom.packages.activatePackage('atom-devquote');
-  });
-
-  describe('when the atom-devquote:toggle event is triggered', () => {
-    it('hides and shows the modal panel', () => {
-      // Before the activation event the view is not on the DOM, and no panel
-      // has been created
-      expect(workspaceElement.querySelector('.atom-devquote')).not.toExist();
-
-      // This is an activation event, triggering it will cause the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'atom-devquote:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        expect(workspaceElement.querySelector('.atom-devquote')).toExist();
-
-        let atomDevquoteElement = workspaceElement.querySelector('.atom-devquote');
-        expect(atomDevquoteElement).toExist();
-
-        let atomDevquotePanel = atom.workspace.panelForItem(atomDevquoteElement);
-        expect(atomDevquotePanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, 'atom-devquote:toggle');
-        expect(atomDevquotePanel.isVisible()).toBe(false);
-      });
+    beforeEach(() => {
+        workspaceElement = atom.views.getView(atom.workspace);
+        activationPromise = atom.packages.activatePackage('atom-devquote');
     });
 
-    it('hides and shows the view', () => {
-      // This test shows you an integration test testing at the view level.
+    describe('when the atom-devquote:toggle event is triggered', () => {
+        it('hides and shows the modal panel', () => {
+            // Before the activation event the view is not on the DOM, and no panel
+            // has been created
+            expect(workspaceElement.querySelector('.atom-devquote')).not.toExist();
 
-      // Attaching the workspaceElement to the DOM is required to allow the
-      // `toBeVisible()` matchers to work. Anything testing visibility or focus
-      // requires that the workspaceElement is on the DOM. Tests that attach the
-      // workspaceElement to the DOM are generally slower than those off DOM.
-      jasmine.attachToDOM(workspaceElement);
+            // This is an activation event, triggering it will cause the package to be
+            // activated.
+            atom.commands.dispatch(workspaceElement, 'atom-devquote:toggle');
 
-      expect(workspaceElement.querySelector('.atom-devquote')).not.toExist();
+            waitsForPromise(() => {
+                return activationPromise;
+            });
 
-      // This is an activation event, triggering it causes the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'atom-devquote:toggle');
+            runs(() => {
+                expect(workspaceElement.querySelector('.atom-devquote')).toExist();
 
-      waitsForPromise(() => {
-        return activationPromise;
-      });
+                let atomDevquoteElement = workspaceElement.querySelector('.atom-devquote');
+                expect(atomDevquoteElement).toExist();
 
-      runs(() => {
-        // Now we can test for view visibility
-        let atomDevquoteElement = workspaceElement.querySelector('.atom-devquote');
-        expect(atomDevquoteElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, 'atom-devquote:toggle');
-        expect(atomDevquoteElement).not.toBeVisible();
-      });
+                let atomDevquotePanel = atom.workspace.panelForItem(atomDevquoteElement);
+                expect(atomDevquotePanel.isVisible()).toBe(true);
+                atom.commands.dispatch(workspaceElement, 'atom-devquote:toggle');
+                expect(atomDevquotePanel.isVisible()).toBe(false);
+            });
+        });
+
+        it('hides and shows the view', () => {
+            // This test shows you an integration test testing at the view level.
+
+            // Attaching the workspaceElement to the DOM is required to allow the
+            // `toBeVisible()` matchers to work. Anything testing visibility or focus
+            // requires that the workspaceElement is on the DOM. Tests that attach the
+            // workspaceElement to the DOM are generally slower than those off DOM.
+            jasmine.attachToDOM(workspaceElement);
+
+            expect(workspaceElement.querySelector('.atom-devquote')).not.toExist();
+
+            // This is an activation event, triggering it causes the package to be
+            // activated.
+            atom.commands.dispatch(workspaceElement, 'atom-devquote:toggle');
+
+            waitsForPromise(() => {
+                return activationPromise;
+            });
+
+            runs(() => {
+                // Now we can test for view visibility
+                let atomDevquoteElement = workspaceElement.querySelector('.atom-devquote');
+                expect(atomDevquoteElement).toBeVisible();
+                atom.commands.dispatch(workspaceElement, 'atom-devquote:toggle');
+                expect(atomDevquoteElement).not.toBeVisible();
+            });
+        });
     });
-  });
 });
--- spec/atom-devquote-view-spec.js
+++ spec/atom-devquote-view-spec.js
@@ -3,7 +3,7 @@
 import AtomDevquoteView from '../lib/atom-devquote-view';
 
 describe('AtomDevquoteView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
+    it('has one valid test', () => {
+        expect('life').toBe('easy');
+    });
 });
