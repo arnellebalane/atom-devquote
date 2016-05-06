e78cfd30ad88838eda8ee56115fc8fb38ee4181b
Arnelle Balane <arnellebalane@gmail.com>
2016-05-06 22:21:18 +0800

    show quote notification, setup menus and keymaps

--- spec/atom-devquote-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomDevquoteView from '../lib/atom-devquote-view';
-
-describe('AtomDevquoteView', () => {
-    it('has one valid test', () => {
-        expect('life').toBe('easy');
-    });
-});
